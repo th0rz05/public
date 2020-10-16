e2482ef1565c4a2adb3cbda5af4e7607906a22c8
Correia Lopes <jlopes@fe.up.pt>
2020-10-16 17:06:37 +0100

    updated lectures/04/tables.py

--- lectures/04/tables.py
+++ lectures/04/tables.py
@@ -7,7 +7,7 @@ Created on  Sun Oct  7 10:29:14 2018
 
 Code adapted from the book:
 
-Peter Wentworth, Jeffrey Elkner, Allen B. Downey, and Chris Meyers, 
+Peter Wentworth, Jeffrey Elkner, Allen B. Downey, and Chris Meyers,
 How to Think Like a Computer Scientist — Learning with Python 3, 2012
 """
 
@@ -15,16 +15,16 @@ How to Think Like a Computer Scientist — Learning with Python 3, 2012
 # tables
 ########################
 
-##### values and 2 raised to the power of that value
+# values and 2 raised to the power of that value
 
-print("n", '\t', "2**n")     #table column headings
+print("n", '\t', "2**n")     # table column headings
 print("---", '\t', "-----")
 
 for x in range(11):          # generate values for columns
     print(x, '\t', 2 ** x)
 
-##### multiples of 2, all on one line
-
+# multiples of 2, all on one line
+print()
 for i in range(1, 7):
     print(2 * i, end="  ")
 print("\nto be continued...")
